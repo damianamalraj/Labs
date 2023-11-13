a24f1e64178cd6edf987ed79c319d757235a7e53
Damian Amalraj <damian.amalraj@hotmail.com>
2023-11-13 23:01:22 +0100

    ✅ DONE: Done with the Lab 10

--- K2/Lab 10/Anropa_databasen_sql_orm/SELECT Products.ProductName, Products.sql
+++ K2/Lab 10/Anropa_databasen_sql_orm/SELECT Products.ProductName, Products.sql
@@ -1,4 +1,18 @@
+-- Hämta alla produkter med deras namn, pris och kategori namn. Sortera på kategori namn och sen produkt namn
 SELECT Products.ProductName, Products.UnitPrice, Categories.CategoryName
 FROM Products
 JOIN Categories ON Products.CategoryID = Categories.CategoryID
 ORDER BY CategoryName, ProductName
+
+-- Hämta alla kunder och antal ordrar de gjort. Sortera fallande på antal ordrar.
+SELECT Customers.CustomerID, Customers.CompanyName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID, Customers.CompanyName
+ORDER BY COUNT(Orders.OrderID) DESC;
+
+-- Hämta alla anställda tillsammans med territorie de har hand om (EmployeeTerritories och Territories tabellerna)
+SELECT e.EmployeeID, e.FirstName, e.LastName, t.TerritoryDescription
+FROM Employees AS e
+JOIN EmployeeTerritories AS et ON e.EmployeeID = et.EmployeeID
+JOIN Territories AS t ON et.TerritoryID = t.TerritoryID;
