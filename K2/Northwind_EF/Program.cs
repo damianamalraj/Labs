7c7ad5a0ab2bd5d9107c06c50170bd12e76e9af0
Damian Amalraj <damian.amalraj@hotmail.com>
2023-12-15 23:23:12 +0100

    🚧 WIP: Routine update

--- K2/Northwind_EF/Program.cs
+++ K2/Northwind_EF/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Northwind_EF.Data;
+using Northwind_EF.Models;
 
 namespace Northwind_EF
 {
@@ -10,7 +12,18 @@ namespace Northwind_EF
     {
         internal static void Main(string[] args)
         {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var ukOrders = context.Orders
+                            .Where(o => o.ShipCountry == "UK")
+                            .Select(o => new { o.ShipAddress, o.ShipName, o.ShipCity })
+                            .ToList();
 
+                foreach (var order in ukOrders)
+                {
+                    Console.WriteLine($"{order.ShipCity}, {order.ShipAddress}, {order.ShipName}");
+                }
+            }
         }
     }
 }
